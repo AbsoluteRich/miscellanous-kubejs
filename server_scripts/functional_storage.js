d5ccb6051dee35fac32599cd5054d6a8812150b3
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-01-17 01:28:00 +1100

    THIS IS HOW I FIX IT??????????

--- server_scripts/functional_storage.js
+++ server_scripts/functional_storage.js
@@ -3,11 +3,11 @@ ServerEvents.recipes((kubejs) => {
         type: "functionalstorage:custom_compacting",
         higher_input: {
             count: 1,
-            id: "minecraft:copper_ingot"
+            item: "minecraft:copper_ingot"
         },
         lower_input: {
             count: 9,
-            id: "create:copper_nugget"
+            item: "create:copper_nugget"
         }
     })
 });
