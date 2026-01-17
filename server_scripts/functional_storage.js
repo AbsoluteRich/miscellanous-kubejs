21d833f8f3b3c692f906b26d303adaca31d09147
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-01-17 12:31:14 +1100

    Prismarine?

--- server_scripts/functional_storage.js
+++ server_scripts/functional_storage.js
@@ -10,4 +10,28 @@ ServerEvents.recipes((kubejs) => {
             item: "create:copper_nugget"
         }
     })
+
+    kubejs.custom({
+        type: "functionalstorage:custom_compacting",
+        higher_input: {
+            count: 1,
+            item: "minecraft:prismarine"
+        },
+        lower_input: {
+            count: 4,
+            item: "minecraft:prismarine_shard"
+        }
+    })
+
+    kubejs.custom({
+        type: "functionalstorage:custom_compacting",
+        higher_input: {
+            count: 1,
+            item: "minecraft:prismarine_bricks"
+        },
+        lower_input: {
+            count: 9,
+            item: "minecraft:prismarine_shard"
+        }
+    })
 });
