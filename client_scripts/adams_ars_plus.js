0e4a9269424e994ca1bf587a19eab3f92a9f0e00
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-01-22 15:43:23 +1100

    Bleh

---
+++ client_scripts/adams_ars_plus.js
@@ -0,0 +1,4 @@
+JEIEvents.hideItems(kubejs => {
+    kubejs.hide("toomanyglyphs:glyph_amplify_two");
+    kubejs.hide("toomanyglyphs:glyph_amplify_three");
+})
--- server_scripts/adams_ars_plus.js
+++ server_scripts/adams_ars_plus.js
@@ -1,93 +1,70 @@
+const frostCost = ["adamsarsplus:frost_soul", 7000];
+const flameCost = ["adamarsplus:flame_soul", 7000];
+const earthSourceCost = ["adamsarsplus:earth_soul", 7000]; // Untested
+const lightningSourceCost = ["adamarsplus:lightning_soul", 8000];
+const holySourceCost = ["adamarsplus:holy_soul", 8000];
+const voidSourceCost = ["adamarsplus:void_soul", 8500];
+
+const replacementMappings = {
+    // Frost
+    "adamsarsplus:cade_hood": ["ars_elemental:aqua_hat", frostCost],
+    "adamsarsplus:cade_robes": ["ars_elemental:aqua_robes", frostCost],
+    "adamsarsplus:cade_leggings": ["ars_elemental:aqua_pants", frostCost],
+    "adamsarsplus:cade_boots": ["ars_elemental:aqua_boots", frostCost],
+
+    // Flame
+    "adamarsplus:ryan_hood": ["arselemental:fire_hat", flameCost],
+    "adamarsplus:ryan_robes": ["arselemental:fire_robes", flameCost],
+    "adamarsplus:ryan_leggings": ["arselemental:fire_pants", flameCost],
+    "adamarsplus:ryan_boots": ["arselemental:fire_boots", flameCost]
+};
+
 ServerEvents.recipes((kubejs) => {
-    // Frost Armour
-    kubejs.remove({ id: "adamsarsplus:cade_hood" })
-    kubejs.remove({ id: "adamsarsplus:cade_robes" })
-    kubejs.remove({ id: "adamsarsplus:cade_leggings" })
-    kubejs.remove({ id: "adamsarsplus:cade_boots" })
+    // Remove duplicate glyphs (these are the more balanced versions, but unfortuately, Ars Plus is preferred for consistency)
+    kubejs.remove({ id: "toomanyglyphs:glyph_amplify_two" });
+    kubejs.remove({ id: "toomanyglyphs:glyph_amplify_three" });
 
-    kubejs.custom({
-        type: "adamsarsplus:a_armor_upgrade",
-        outTier: 3,
-        output: {
-            item: "adamsarsplus:cade_hood"
-        },
-        pedestalItems: [
-            {
-                item: {
-                    item: "adamsarsplus:frost_soul"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:frost_soul"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:frost_soul"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:frost_soul"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:mana_diamond"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:mana_diamond"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:mana_diamond"
-                }
-            },
-            {
-                item: {
-                    item: "adamsarsplus:mana_diamond"
-                }
-            }
-        ],
-        reagent: [
-            {
-                item: "ars_elemental:aqua_hat"
-            }
-        ],
-        sourceCost: 7000,
-        tier: 3
-    })
+    // Add Ars Elemental to the Ars Plus armour progression
+    let replacements = Object.entries(replacementMappings);
+    replacements.forEach((mapping) => {
+        let oldItem = mapping[0];
+        let newItem = mapping[1][0];
+        let soul = mapping[1][1][0];
+        let sourceCost = mapping[1][1][1];
 
-    kubejs.custom(
-        {
+        console.log("----")
+        console.log(oldItem)
+        console.log(newItem)
+        console.log(soul)
+        console.log(sourceCost)
+
+        kubejs.remove({ id: oldItem });
+
+        kubejs.custom({
             type: "adamsarsplus:a_armor_upgrade",
             outTier: 3,
             output: {
-                item: "adamsarsplus:cade_robes"
+                item: oldItem
             },
             pedestalItems: [
                 {
                     item: {
-                        item: "adamsarsplus:frost_soul"
+                        item: soul
                     }
                 },
                 {
                     item: {
-                        item: "adamsarsplus:frost_soul"
+                        item: soul
                     }
                 },
                 {
                     item: {
-                        item: "adamsarsplus:frost_soul"
+                        item: soul
                     }
                 },
                 {
                     item: {
-                        item: "adamsarsplus:frost_soul"
+                        item: soul
                     }
                 },
                 {
@@ -113,11 +90,11 @@ ServerEvents.recipes((kubejs) => {
             ],
             reagent: [
                 {
-                    item: "ars_elemental:aqua_robes"
+                    item: newItem
                 }
             ],
-            sourceCost: 7000,
+            sourceCost: sourceCost,
             tier: 3
-        }
-    )
+        })
+    });
 });
