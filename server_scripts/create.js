7caca02fb3b06e0a159d984799fe5fdbbcc73f27
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-02-08 00:34:54 +1100

    Honey fixing

--- server_scripts/create.js
+++ server_scripts/create.js
@@ -1,6 +1,16 @@
 ServerEvents.recipes((kubejs) => {
-    kubejs.recipes.createCompacting(
-        "minecraft:honeycomb", // Input(s)
-        Fluid.of("create:honey", 250),  // Output(s), replace this with #forge:honey
+    // With these recipes, you can produce slightly more honeycomb by pumping honey from beehives compared to just shearing them regularly
+    // This may be a balancing concern?? Honey has so little uses I don't feel like implementing a solution
+    
+    // Produce honey from honeycomb
+    kubejs.recipes.create.mixing(
+        Fluid.of("create:honey", 250),  // Output(s)
+        "minecraft:honeycomb"  // Input(s), replace this with #forge:honey
+    ).heated()
+
+    // Produce honeycomb from honey
+    kubejs.recipes.create.compacting(
+        "minecraft:honeycomb",
+        Fluid.of("create:honey", 250),
     )
 });
