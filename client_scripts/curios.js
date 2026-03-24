68b62db8338c36429bb49a77701c9de29a658e84
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-03-24 15:37:01 +1100

    Curious

--- client_scripts/curios.js
+++ client_scripts/curios.js
@@ -1,6 +1,11 @@
 ItemEvents.tooltip(event => {
     event.add([
         "create:copper_backtank",
+        "mekanism:jetpack",
+        "mekanism:free_runners",
+    ], "Upgrade these items to be able to equip them in your Curios slot.")
+
+    event.add([
         "create:netherite_backtank",
         "mekanism:jetpack_armored",
         "mekanism:free_runners_armored",
