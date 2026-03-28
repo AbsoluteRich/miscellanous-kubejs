b51ef811a1598a0376c661fcd889edc36c226ed8
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-03-28 21:04:45 +1100

    Crushing compat

--- server_scripts/mekanism.js
+++ server_scripts/mekanism.js
@@ -36,4 +36,43 @@ ServerEvents.recipes((kubejs) => {
             item: "2x minecraft:sugar"
         }
     })
+
+    // Biofuel recipes for modded crops
+    // Seeds
+    let crushableCrops = {
+        // Todo: Ars logs and Ars fruits, Farmer's Delight mushrooms and wild bushes, Twilight saplings and leaves, Tinkers saplings and leaves
+        2: [
+            "supplementaries:flax_seeds",
+            "ars_nouveau:magebloom_crop",
+            "ars_nouveau:sourceberry_bush",
+            "farmersdelight:rice",
+            "farmersdelight:rice_panicle",
+            "farmersdelight:cabbage_seeds",
+            "farmersdelight:tomato_seeds",
+        ],
+        5: [
+            "supplementaries:flax",
+            "ars_nouveau:magebloom",
+            "alexsmobs:banana",
+            "farmersdelight:cabbage",
+            "farmersdelight:tomato",
+            "farmersdelight:onion",
+        ]
+    }
+
+    crushableCrops.forEach(biofuelCount, crops => {
+        crops.forEach(item_id => {
+            kubejs.custom({
+                type: "mekanism:crushing",
+                input: {
+                    ingredient: {
+                        item: item_id
+                    }
+                },
+                output: {
+                    item: Item.of("mekanism_bio_fuel", biofuelCount)
+                }
+            })
+        })
+    })
 })
